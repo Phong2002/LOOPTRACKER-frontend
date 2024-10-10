117a49ae1b8496426bfa1fd14c346f43dd883ed3
Lù Minh Phong <PhongLM@vissoft.vn>
2024-10-10 20:28:59 +0700

    update: add path

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,12 @@ import RegistrationRequestPage from "./page/auth/RegistrationRequestPage.jsx";
 import {Provider} from "react-redux";
 import {store} from './redux/app/Store.jsx';
 import MainLayout from "./page/layout/MainLayout.jsx";
-import Home from "./page/Home.jsx";
+import HomePage from "./page/HomePage.jsx";
+import PassengerPage from "./page/PassengerPage.jsx";
+import TourPackagePage from "./page/TourPackagePage.jsx";
+import TourInstancePage from "./page/TourInstancePage.jsx";
+import ItemPage from "./page/ItemPage.jsx";
+import UserManagementPage from "./page/UserManagementPage.jsx";
 
 function App() {
   return (
@@ -14,7 +19,13 @@ function App() {
         <Provider store={store}>
         <Routes>
             <Route path="" element={<MainLayout/>}>
-                <Route index element={<Home />} />
+                <Route index element={<HomePage />} />
+                <Route path="passenger" element={<PassengerPage/>} />
+                <Route path="tour-package" element={<TourPackagePage/>} />
+                <Route path="tour-instance" element={<TourInstancePage/>} />
+                <Route path="item" element={<ItemPage/>} />
+                <Route path="user-management" element={<UserManagementPage/>} />
+
             </Route>
             <Route path="/login" element={<LoginPage />} />
             <Route path="/registration-request" element={<RegistrationRequestPage />} />
